569f247a50faa4e4882132b5ed3ce9f0b1285cfb
Jonathon F <59483067+jwfreeland1@users.noreply.github.com>
2020-05-02 13:49:35 -0600

    Added error handling
    Added error catch for missing Role ID

--- commands/region.js
+++ commands/region.js
@@ -7,31 +7,38 @@ module.exports = {
     usage: '<add/remove> <region>',
 	execute(message, args) {
 
-        const array = fs.readFileSync(process.cwd() + "/commands/roles.txt").toString();
-        const region = args[1].toString().toUpperCase();
-        let role = message.guild.roles.cache.find(role => role.name === region);
+        const json = fs.readFileSync(process.cwd() + "/commands/roles.json");
+        const array = fs.readFileSync(process.cwd() + "/commands/regions.txt").toString();
+        let regionData = JSON.parse(json);
+        let userRegion = args[1].toString().toUpperCase();
 
+        console.log(regionData[userRegion]);
 
         // If user DMs the bot it will send an error message
         if (message.channel.type == 'dm')
         return message.author.send('Please use the #bot-commands channel to interact with me.');
         // Handle permission error for when owner or mod attempts to use command
         if (message.author.id === message.guild.ownerID)
-        return message.author.send('I am terribly sorry about this, but I am unable to change the nickname of a server owner.');
+        return message.author.send('I am terribly sorry about this, but I am unable to change the role of a server owner.');
         if (message.member.roles.cache.has('700052590632763443'))
-        return message.author.send('I am terribly sorry about this, but I am unable to change the nickname of a mod.');
+        return message.author.send('I am terribly sorry about this, but I am unable to change the role of a mod.');
         // Check whether user is submitting an add or remove command then check against array to ensure it's valid
         if (args[0].toLowerCase() === 'add') {
-            if (array.includes(region)) {
-                message.member.roles.add(role);
-                message.author.send('I have added you to the ' + region + ' group.');
-            } else return message.author.send('**ERROR!** That is not a valid Region');
-        }
-        else if (args[0] === 'remove'){
-            if (array.includes(region)) { array.
-                message.member.roles.remove(role);
-                message.author.send('I have removed you from the ' + region + ' group.');
-            } else return message.author.send('**ERROR!** That is not a valid Region');
+            if (array.includes(userRegion)) {
+                message.member.roles.add(regionData[userRegion]).catch(e => {
+                    console.error('ERROR: Role ID does not exist');
+                    message.author.send('**ERROR!** That is not a valid Region');
+                    return;
+                });
+            } else message.author.send('I have added you to the ' + userRegion + ' group.');
+        } else if (args[0].toLowerCase() === 'remove'){
+            if (array.includes(userRegion)) {
+                message.member.roles.remove(regionData[userRegion]).catch(e => {
+                    console.error('ERROR: Role ID does not exist');
+                    message.author.send('**ERROR!** That is not a valid Region');
+                    return;
+                });
+            } else message.author.send('I have removed you from the ' + userRegion + ' group.');
         }
     }
 };
